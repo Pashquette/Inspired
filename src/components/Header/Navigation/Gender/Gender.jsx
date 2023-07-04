f6c0ff0040a0d68705f6d6c8d014c393417d6a7e
Pavel <jrmarty@mail.ru>
2023-07-04 14:57:30 +0300

    day 2

--- src/components/Header/Navigation/Gender/Gender.jsx
+++ src/components/Header/Navigation/Gender/Gender.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import s from './Gender.module.scss'
 import cn from 'classnames'
 
@@ -6,13 +6,16 @@ const list = [
     {link: 'women', title: 'Женщины'},
     {link: 'men', title: 'Мужчины'},
 ]
+
 export const Gender = () => {
+    const location = useLocation()
+    console.log(location);
     return (
        <ul className={s.gender}>
-        {list.map(item => {
+        {list.map((item, index) => {
             return (
                 <li className={s.item} key={item.link}>
-                    <NavLink end className={({isActive}) => cn(s.link, isActive && s.linkActive)} to={item.link}>
+                    <NavLink className={({isActive}) => cn(s.link, isActive && s.linkActive)} to={item.link}>
                         {item.title}
                     </NavLink>
                 </li>
