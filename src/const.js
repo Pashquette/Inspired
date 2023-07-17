5ce109a2afb9b12d708ed04ef7059f16f98de373
Pavel <jrmarty@mail.ru>
2023-07-17 15:47:31 +0300

    fix server

--- src/const.js
+++ src/const.js
@@ -1,7 +1,9 @@
-export const API_URL = 'http://localhost:8024';
+export const API_URL = 'https://elated-awake-ophthalmologist.glitch.me';
 export const GOODS_URL = `${API_URL}/api/goods`
 export const CATEGORY_URL = `${API_URL}/api/categories`
 export const COLORS_URL = `${API_URL}/api/colors`
 export const ORDER_URL = `${API_URL}/api/order`
 
-// https://elated-awake-ophthalmologist.glitch.me
+// 
+
+// http://localhost:8024
